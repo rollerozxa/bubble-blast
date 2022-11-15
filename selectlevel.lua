5c0e5eac7e0ed6457b9144c7044cda181895bb8a
ROllerozxa <rollerozxa@voxelmanip.se>
2022-11-15 22:47:35 +0100

    Hover effect on level select

--- selectlevel.lua
+++ selectlevel.lua
@@ -33,7 +33,7 @@ function scenes.selectlevel.draw()
 	end
 
 	love.graphics.setFont(fonts.sans.medium)
-	love.graphics.print("Page: "..scenes.selectlevel.page, ScaledX(140), ScaledY(64), 0, ScaledX(), ScaledY())
+	love.graphics.print("Page: "..scenes.selectlevel.page, ScaledX(136), ScaledY(69), 0, ScaledX(), ScaledY())
 	love.graphics.setFont(fonts.sans.small)
 
 	if scenes.selectlevel.page ~= 4 then
@@ -45,7 +45,12 @@ function scenes.selectlevel.draw()
 		local y = math.floor(i / 5)
 		local levelnum = ((scenes.selectlevel.page - 1) * 25 ) + i + 1
 
-		love.graphics.setColor(0.1,0.1,0.1)
+		if MouseCollisionScaled(x * 64 - 32, 128 + y * 64, 32, 32) then
+			love.graphics.setColor(0,0,0.1)
+		else
+			love.graphics.setColor(0.1,0.1,0.1)
+		end
+
 		love.graphics.rectangle('fill', ScaledX(x * 64 - 32), ScaledY(128 + y * 64), ScaledX(32), ScaledY(32))
 
 		love.graphics.setColor(1,1,1)
